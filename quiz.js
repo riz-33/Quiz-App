274af00526035e3af6cbeff1822246bd543281c1
rizwan <118995669+riz-33@users.noreply.github.com>
2024-06-07 00:47:05 +0500

    update

--- quiz.js
+++ quiz.js
@@ -1,96 +1,112 @@
 var questions = [
     {
         question: "Who inaugurated the State Bank of Pakistan?",
-        answers: [
-            { text: "Quaid-e-Azam", correct: "true" },
-            { text: "Malik Ghulam Muhammad", correct: "false" },
-            { text: "Liaquat Ali Khan", correct: "false" },
-            { text: "Allama Muhammad Iqbal", correct: "false" },
-        ]
+        option1: "Quaid-e-Azam",
+        option2: "Malik Ghulam Muhammad",
+        option3: "Liaquat Ali Khan",
+        option4: "Allama Muhammad Iqbal",
+        answer: "Quaid-e-Azam",
     },
     {
         question: "Sardar Abdur Rub Nishtar was the Governor of?",
-        answers: [
-            { text: "Gilgit Baltistan", correct: "false" },
-            { text: "KPK", correct: "false" },
-            { text: "Punjab", correct: "true" },
-            { text: "Sindh", correct: "false" },
-        ]
+        option1: "Gilgit Baltistan",
+        option2: "KPK",
+        option3: "Punjab",
+        option4: "Sindh",
+        answer: "Punjab",
     },
     {
         question: "National code of Pakistan is?",
-        answers: [
-            { text: "PAK", correct: "false" },
-            { text: "PK", correct: "true" },
-            { text: "PAK 1", correct: "false" },
-            { text: "None of them", correct: "false" },
-        ]
+        option1: "PAK",
+        option2: "PK",
+        option3: "PAK 1",
+        option4: "None of them",
+        answer: "PK",
     },
     {
         question: "Where is Warsak Dam of Pakistan situated?",
-        answers: [
-            { text: "Khyber Pakhtunkhwa", correct: "true" },
-            { text: "Punjab", correct: "false" },
-            { text: "Sindh", correct: "false" },
-            { text: "Balochistan", correct: "false" },
-        ]
+        option1: "Khyber Pakhtunkhwa",
+        option2: "Punjab",
+        option3: "Sindh",
+        option4: "Balochistan",
+        answer: "Khyber Pakhtunkhwa",
     },
     {
         question: "Shakarparrian is situated in?",
-        answers: [
-            { text: "Islamabad", correct: "true" },
-            { text: "Rawalpindi", correct: "false" },
-            { text: "Murree", correct: "false" },
-            { text: "Peshawar", correct: "false" },
-        ]
+        option1: "Islamabad",
+        option2: "Rawalpindi",
+        option3: "Murree",
+        option4: "Peshawar",
+        answer: "Islamabad",
     },
     {
         question: "Identify the largest cantonment of Pakistan?",
-        answers: [
-            { text: "Kharian Cantt", correct: "true" },
-            { text: "Quetta Cantt", correct: "false" },
-            { text: "Okara Cantt", correct: "false" },
-            { text: "Karachi Cantt", correct: "false" },
-        ]
+        option1: "Kharian Cantt",
+        option2: "Quetta Cantt",
+        option3: "Okara Cantt",
+        option4: "Karachi Cantt",
+        answer: "Kharian Cantt",
     },
     {
         question: "Which city of Pakistan held the OIC Conference in 1997?",
-        answers: [
-            { text: "Lahore", correct: "false" },
-            { text: "Islamabad", correct: "true" },
-            { text: "Karachi", correct: "false" },
-            { text: "Peshawar", correct: "false" },
-        ]
+        option1: "Lahore",
+        option2: "Islamabad",
+        option3: "Karachi",
+        option4: "Peshawar",
+        answer: "Islamabad",
     },
     {
         question: "In which year was OIC founded?",
-        answers: [
-            { text: "1970", correct: "false" },
-            { text: "1975", correct: "false" },
-            { text: "1980", correct: "false" },
-            { text: "1969", correct: "true" },
-        ]
+        option1: "1970",
+        option2: "1975",
+        option3: "1980",
+        option4: "1969",
+        answer: "1969",
     },
     {
         question: "The Kargil incident happened in?",
-        answers: [
-            { text: "1998", correct: "false" },
-            { text: "1997", correct: "false" },
-            { text: "1999", correct: "true" },
-            { text: "2000", correct: "false" },
-        ]
+        option1: "1998",
+        option2: "1997",
+        option3: "1999",
+        option4: "2000",
+        answer: "1999",
     },
     {
         question: "Which country assisted Pakistan in the construction of Sandak Project?",
-        answers: [
-            { text: "Iran", correct: "false" },
-            { text: "Saudi Arabia", correct: "false" },
-            { text: "Afghanistan", correct: "false" },
-            { text: "China", correct: "true" },
-        ]
+        option1: "Iran",
+        option2: "Saudi Arabia",
+        option3: "Afghanistan",
+        option4: "China",
+        answer: "China",
     }
 ];
 
-var question = document.getElementById ("question");
-var answer = document.getElementById ("answer");
-var next = document.getElementById ("next-btn");
+// var answer = document.getElementById("answer");
+// var next = document.getElementById("next-btn");
+
+var index = 0;
+var score =0;
+
+function loadQuestion() {
+    var quiz = document.getElementById("qna");
+    var options = document.getElementsByName("option");
+    for (var i = 0; i < options.length; i++) {
+        if (options[i].checked) {
+            console.log(options[i].value)
+        }
+    }
+
+
+    quiz.innerHTML = `
+    <h2 id="question">${questions[index].question}</h2>
+    <div id="answer" class="answer">
+    <div class="ans-btn"><label><input type="radio" name="option" value="${questions[index].option1}" > ${questions[index].option1}</label></div>
+    <div class="ans-btn"><label><input type="radio" name="option" value="${questions[index].option2}" > ${questions[index].option2}</label></div>
+    <div class="ans-btn"><label><input type="radio" name="option" value="${questions[index].option3}" > ${questions[index].option3}</label></div>
+    <div class="ans-btn"><label><input type="radio" name="option" value="${questions[index].option4}" > ${questions[index].option4}</label></div>  
+    `
+    index++
+}
+
+
+loadQuestion()
